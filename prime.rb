9d1888c57c96781aee7628c8f6119890b341a407
William Chabla <w_chabla@hotmail.com>
2018-06-21 18:12:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(integer)
   
   divisors = (integer / 2).ceil
-  divisors_array = range(1..divisors).to_a
+  divisors_array = (1..divisors).to_a
   
   divisors_array.each do |divisor|
     if integer%divisor != 0
